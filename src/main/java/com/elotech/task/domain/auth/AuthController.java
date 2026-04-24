c93336a74b43884179f2984bbc702ca077cbb5b6
Johny Kerstin <johnykrc@gmail.com>
2026-04-24 17:30:13 -0300

    fix warnings

--- src/main/java/com/elotech/task/domain/auth/AuthController.java
+++ src/main/java/com/elotech/task/domain/auth/AuthController.java
@@ -24,7 +24,7 @@ public class AuthController {
     }
 
     @PostMapping
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody @Valid AuthenticationDTO data){
         var authenticationToken = new UsernamePasswordAuthenticationToken(data.email(), data.password());
 
         var auth = authenticationManager.authenticate(authenticationToken);
