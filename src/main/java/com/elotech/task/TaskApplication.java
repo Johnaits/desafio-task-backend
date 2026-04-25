1f1215d72d189d9f3011f84c9c769c773238add1
Johny Kerstin <johnykrc@gmail.com>
2026-04-25 12:29:04 -0300

    - feat enable jpa auditing to @CreatedDate

--- src/main/java/com/elotech/task/TaskApplication.java
+++ src/main/java/com/elotech/task/TaskApplication.java
@@ -2,7 +2,9 @@ package com.elotech.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //@CreatedDate
 @SpringBootApplication
 public class TaskApplication {
 
