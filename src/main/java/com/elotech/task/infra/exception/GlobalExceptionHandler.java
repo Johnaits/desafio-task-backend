82fcaa60d36b019cc5ef77988f3bdec36b10ef4b
Johny Kerstin <johnykrc@gmail.com>
2026-04-25 12:24:52 -0300

    - feat exception handler for entity not found

--- src/main/java/com/elotech/task/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/elotech/task/infra/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.elotech.task.infra.exception;
 
 import com.elotech.task.infra.exception.dto.DefaultErrorDTO;
 import com.elotech.task.infra.exception.dto.FieldErrorDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,11 +26,21 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<FieldErrorDTO>> handleValidationErros(MethodArgumentNotValidException ex){
+    public ResponseEntity<List<FieldErrorDTO>> handleValidationErrors(MethodArgumentNotValidException ex){
         List<FieldErrorDTO> errors = ex.getFieldErrors().stream()
                 .map(FieldErrorDTO::new)
                 .toList();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<DefaultErrorDTO> handleEntityNotFound(EntityNotFoundException ex){
+        var error = new DefaultErrorDTO(
+                HttpStatus.NOT_FOUND.value(),
+                "Não encontrado",
+                "Entidade inexistente"
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
