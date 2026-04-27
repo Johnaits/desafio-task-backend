797adca3db3d8e265205e01a626ecf2c7f5b765f
Johny Kerstin <johnykrc@gmail.com>
2026-04-27 14:58:21 -0300

    fix ProblemDetail pattern

--- src/main/java/com/elotech/task/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/elotech/task/infra/exception/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.elotech.task.infra.exception;
 
-import com.elotech.task.infra.exception.dto.DefaultErrorDTO;
 import com.elotech.task.infra.exception.dto.FieldErrorDTO;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,31 +16,46 @@ import java.util.List;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<DefaultErrorDTO> handleBadCredentials(BadCredentialsException ex){
-        var error = new DefaultErrorDTO(
-                HttpStatus.UNAUTHORIZED.value(),
-                "Não autorizado",
-                "E-mail ou senha incorretos"
-        );
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    public ProblemDetail handleBadCredentials(BadCredentialsException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, "E-mail ou senha incorretos");
+        problemDetail.setTitle("Não autorizado");
+        return problemDetail;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<FieldErrorDTO>> handleValidationErrors(MethodArgumentNotValidException ex){
+    public ProblemDetail handleValidationErrors(MethodArgumentNotValidException ex){
+
         List<FieldErrorDTO> errors = ex.getFieldErrors().stream()
                 .map(FieldErrorDTO::new)
                 .toList();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,"Argumentos inválidos. Verifique o(s) campo(s) passados");
+        problemDetail.setTitle("Erro de validação de dados");
+        problemDetail.setProperty("invalidFields", errors);
+        return problemDetail;
 
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<DefaultErrorDTO> handleEntityNotFound(EntityNotFoundException ex){
-        var error = new DefaultErrorDTO(
-                HttpStatus.NOT_FOUND.value(),
-                "Não encontrado",
-                "Entidade inexistente"
-        );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    public ProblemDetail handleEntityNotFound(EntityNotFoundException ex){
+        String message = (ex.getMessage() != null) ? ex.getMessage() : "Entidade inexistente";
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, message);
+        problemDetail.setTitle("Não encontrado");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ProblemDetail handleIllegalArgument(IllegalArgumentException ex){
+        String message = (ex.getMessage() != null) ? ex.getMessage() : "Erro de validação nos dados fornecidos.";
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, message);
+        problemDetail.setTitle("Argumento inválido");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ProblemDetail handleViolationData(DataIntegrityViolationException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Violação na integridade dos dados");
+        problemDetail.setTitle("Dado inválido");
+        return problemDetail;
     }
 }
