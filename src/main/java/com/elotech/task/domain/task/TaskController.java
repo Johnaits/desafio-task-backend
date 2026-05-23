973586588008a24baee818c3b062675f820466da
Johny Kerstin <johnykrc@gmail.com>
2026-05-23 18:27:51 -0300

    feat: roles ADMIN and MEMBER

--- src/main/java/com/elotech/task/domain/task/TaskController.java
+++ src/main/java/com/elotech/task/domain/task/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@PreAuthorize("hasAnyRole('ADMIN', 'MEMBER')")
 public class TaskController {
 
     private final TaskService taskService;
